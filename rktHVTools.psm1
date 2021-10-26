bc895271ff66e19b86ddf4833ad3941f542d3d67
rkt6865 <robert_terra@brown.edu>
2021-10-26 10:55:51 -0400

    Add updated Get-HVLldpInfo and Get-HVHostNicInfo functions

--- rktHVTools.psm1
+++ rktHVTools.psm1
@@ -382,28 +382,28 @@ function Get-HVHostHardware {
 ###################################
 function Get-HVLldpInfo {
     <#
-    .SYNOPSIS
-        Retrieve physical switch and port information for each interface of a Hyper-V host.
-    .DESCRIPTION
-        Retrieves the MAC address, physical switch name, physical switch port, and port description for each network
-        interface of a Hyper-V host.
-    .PARAMETER hostName
-        Specifies the name of the Hyper-v host. This parameter is mandatory.
-    .PARAMETER Refresh
-        Refresh the LLDP information for the interface. Optional.
-    .INPUTS
-        System.String.  Get-HVLldpInfo accepts a string as the name of the Hyper-V host.
-    .OUTPUTS
-        PSCustomObject. Get-HVLldpInfo returns the host name, MAC, connection state, speed, physical switch name, and physical port.
-    .EXAMPLE
-        PS C:\> Get-HVLldpInfo <myVMHostName>
-        Retrieves the physical switch and port information for each interface of the Hyper-V host <myVMHostName>.
-    .EXAMPLE
-        PS C:\> Get-HVLldpInfo -Refresh <myVMHostName>
-        Retrieves the physical switch/port information after first retrieving the information from the physical switch.
-    .NOTES
-        If the interface returns no information, try applying the -Refresh switch.  This will reach out to the switch to gather the information.
-    #>
+.SYNOPSIS
+    Retrieve physical switch and port information for each interface of a Hyper-V host.
+.DESCRIPTION
+    Retrieves the MAC address, physical switch name, physical switch port, and port description for each network
+    interface of a Hyper-V host.
+.PARAMETER hostName
+    Specifies the name of the Hyper-v host. This parameter is mandatory.
+.PARAMETER Refresh
+    Refresh the LLDP information for the interface. Optional.
+.INPUTS
+    System.String.  Get-HVLldpInfo accepts a string as the name of the Hyper-V host.
+.OUTPUTS
+    PSCustomObject. Get-HVLldpInfo returns the host name, MAC, connection state, speed, physical switch name, and physical port.
+.EXAMPLE
+    PS C:\> Get-HVLldpInfo <myVMHostName>
+    Retrieves the physical switch and port information for each interface of the Hyper-V host <myVMHostName>.
+.EXAMPLE
+    PS C:\> Get-HVLldpInfo -Refresh <myVMHostName>
+    Retrieves the physical switch/port information after first retrieving the information from the physical switch.
+.NOTES
+    If the interface returns no information, try applying the -Refresh switch.  This will reach out to the switch to gather the information.
+#>
     [CmdletBinding()]
     param (
         [Parameter(
@@ -418,17 +418,17 @@ function Get-HVLldpInfo {
         [Switch]
         $Refresh
     )
-        
+    
     begin {
         if (!(Test-Path Env:\vmm_server)) {
             Write-Host "The following Environment variable needs to be set prior to running the script:" -ForegroundColor Yellow
             Write-Host "`$Env:vmm_server = <vmm server>" -ForegroundColor Yellow
             break
         }
-    
+
         $vmmserver = Get-SCVMMServer $Env:vmm_server
     }
-        
+    
     process {
         $hvHost = Get-SCVMHost -VMMServer $vmmserver -ComputerName $hostName -ErrorAction SilentlyContinue
         if (! $hvHost) {
@@ -448,26 +448,26 @@ function Get-HVLldpInfo {
             }
             $hshNicProps = [ordered]@{
                 Host       = $hvHost.Name.Split(".")[0]
+                Cluster    = $hvhost.HostCluster.Name.Split(".")[0]
                 MAC        = $nic.MacAddress
                 State      = $nic.ConnectionState
                 MaxSpeed   = $nic.MaxBandwidth
                 pSwitch    = $nic.LldpInformation.SystemName
                 pPort      = $nic.LldpInformation.PortId
-                #Desc       = $nic.LldpInformation.PortDescription
+                Desc       = $nic.LldpInformation.PortDescription
                 lastUpdate = $nic.LldpInformation.UpdatedTimestamp
             }
             #$nicPropsArr += New-Object -type PSCustomObject -Property $hshNicProps
             New-Object -type PSCustomObject -Property $hshNicProps
-                
-        }
-                    
-    }
-        
-    end {
             
+        }
+                
+    }
+    
+    end {
+        
     }
 }
-    
 ###################################
 ###################################
 function Get-HVWWN {
@@ -784,12 +784,13 @@ function Get-HVHostNicInfo {
         Remove-CimSession -CimSession $cimSession
         foreach ($nic in $nics) {
             $hshNICProperties = [ordered]@{
-                Name      = $hostName
-                NIC       = $nic.Name
-                NICDesc     = $nic.InterfaceDescription
-                MAC  = $nic.MacAddress
-                Speed       = $nic.LinkSpeed
-                Status   = $nic.Status
+                Name    = $hostName
+                NIC     = $nic.Name
+                NICDesc = $nic.InterfaceDescription
+                MAC     = $nic.MacAddress
+                MTU     = $nic.MtuSize
+                Speed   = $nic.LinkSpeed
+                Status  = $nic.Status
             }
             New-Object -type PSCustomObject -Property $hshNICProperties
 
